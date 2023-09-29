2f635a540f708acdf5bbe356c3de8709c4ce92cf
秦晨 <qsc1956826@Gmail.com>
2023-09-29 13:19:45 +0800

    将删除动画从原先的直接隐藏内容改为透明度渐变效果

--- app/javascript/controllers/removals_controller.js
+++ app/javascript/controllers/removals_controller.js
@@ -2,17 +2,29 @@ import {Controller} from "@hotwired/stimulus"
 
 // Connects to data-controller="removals"
 export default class extends Controller {
-    remove() {
-        this.element.remove()
+    connect() {
+        this.load = 0
+    }
+
+    blurring(target) {
+        console.log("blur")
+        this.load++
+        if (this.load > 10) {
+            clearInterval(this.int)
+        }
+        target.style.opacity = this.scale(this.load, 0, 10, 1, 0)
     }
 
     fade(event) {
         event.preventDefault()
         const targetElement = this.element.parentNode.parentElement
+        this.int = setInterval(() => {
+            this.blurring(targetElement)
+        }, 30)
         targetElement.style.height = getComputedStyle(targetElement).height
-        targetElement.style.visibility = "hidden";
         targetElement.style.margin = 0;
         targetElement.style.padding = 0;
+
         targetElement.addEventListener("transitionend", async () => {
             let form = this.element.parentElement;
             const formData = new FormData(form);
@@ -31,8 +43,18 @@ export default class extends Controller {
                 console.log("表单提交失败:", error);
             }
         })
+
         setTimeout(() => {
             targetElement.style["height"] = 0;
         }, 0);
+    }
+
+    remove() {
+        this.element.remove()
+    }
+
+    // https://stackoverflow.com/questions/10756313/javascript-jquery-map-a-range-of-numbers-to-another-range-of-numbers
+    scale = (num, in_min, in_max, out_min, out_max) => {
+        return ((num - in_min) * (out_max - out_min)) / (in_max - in_min) + out_min
     }
 }

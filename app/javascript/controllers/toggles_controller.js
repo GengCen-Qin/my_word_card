71e6fcfed01e3a2c8b14c63b8e1d37532f147d26
秦晨 <qsc1956826@Gmail.com>
2023-09-29 23:13:02 +0800

    引入弹框layer概念

--- app/javascript/controllers/toggles_controller.js
+++ app/javascript/controllers/toggles_controller.js
@@ -1,8 +1,20 @@
-import { Controller } from "@hotwired/stimulus"
+import {Controller} from "@hotwired/stimulus"
 
 // Connects to data-controller="toggles"
 export default class extends Controller {
-  toggle() {
-    this.element.parentNode.classList.toggle("active")
-  }
+    connect() {
+        const openLayerBtn = document.getElementById("openLayer");
+        openLayerBtn.addEventListener("click", () => {
+            document.getElementById("layer").style.display = "flex"
+        })
+
+        const closeLayBtn = document.getElementById("closeLayer");
+        closeLayBtn.addEventListener("click",() => {
+            document.getElementById("layer").style.display = "none"
+        })
+    }
+
+    toggle() {
+        this.element.parentNode.classList.toggle("active")
+    }
 }

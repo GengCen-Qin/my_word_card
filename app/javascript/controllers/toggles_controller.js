140eea8dbf7911cb423c421e30e9f409ca6f35b0
秦晨 <qsc1956826@Gmail.com>
2023-09-30 11:59:45 +0800

    增加弹出框layer效果

--- app/javascript/controllers/toggles_controller.js
+++ app/javascript/controllers/toggles_controller.js
@@ -2,19 +2,20 @@ import {Controller} from "@hotwired/stimulus"
 
 // Connects to data-controller="toggles"
 export default class extends Controller {
-    connect() {
-        const openLayerBtn = document.getElementById("openLayer");
-        openLayerBtn.addEventListener("click", () => {
-            document.getElementById("layer").style.display = "flex"
-        })
-
-        const closeLayBtn = document.getElementById("closeLayer");
-        closeLayBtn.addEventListener("click",() => {
-            document.getElementById("layer").style.display = "none"
-        })
-    }
-
     toggle() {
         this.element.parentNode.classList.toggle("active")
+    }
+
+    async aiMakeMessage() {
+        const response = await fetch("/word/makeMessageByAI", {method: "GET",});
+        const aiMessage = await response.text();
+        const layerContent = document.getElementById("layerContent");
+        layerContent.innerText = aiMessage
+        document.getElementById("layer").style.display = "flex"
+    }
+
+    closeLayer() {
+        const closeLayBtn = document.getElementById("closeLayer");
+        document.getElementById("layer").style.display = "none"
     }
 }
